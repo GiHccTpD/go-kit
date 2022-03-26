81d63c10830d89f9dd942062184ab5a32297f41b
wangxiao <@FiuLtCZh8AYqa.6DqAJ>
2022-03-26 19:28:42 +0800

    add config methods

--- config/config.go
+++ config/config.go
@@ -1,7 +1,9 @@
-package config
+package Config
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"log"
 
 	"github.com/spf13/viper"
@@ -18,4 +20,66 @@ func LoadConfigFile(cfgFile string) error {
 		}
 	}
 	return err
+}
+
+func MustLoadConfigFile(cfgFile string) {
+	if err := LoadConfigFile(cfgFile); err != nil {
+		panic(fmt.Errorf("Fatal error config file: %w \n", err))
+	}
+}
+
+func LoadConfigByte(data []byte, filetype string) error {
+	var err error
+
+	switch filetype {
+	case "yaml":
+	case "yml":
+	case "toml":
+	case "json":
+	default:
+		err = fmt.Errorf("file ext not support")
+	}
+
+	if err != nil {
+		return err
+	}
+
+	viper.SetConfigType(filetype)
+	err = viper.ReadConfig(bytes.NewBuffer(data))
+
+	return err
+}
+
+func MustLoadConfigByte(data []byte, filetype string) {
+	if err := LoadConfigByte(data, filetype); err != nil {
+		panic(fmt.Errorf("Fatal error config file: %w \n", err))
+	}
+}
+
+// MergeConfig 合并配置 byte
+func MergeConfig(byteCfg io.Reader) error {
+	return viper.MergeConfig(byteCfg)
+}
+
+// MergeConfigWithPath 合并配置 文件路径 cfgPath 文件夹路径
+func MergeConfigWithPath(cfgPath string) error {
+	// 追加一份配置
+	viper.AddConfigPath(cfgPath)
+	err := viper.MergeInConfig() // Find and read the config file
+	if err != nil {              // Handle errors reading the config file
+		return fmt.Errorf("Fatal error config file: %w \n", err)
+	} else {
+		return nil
+	}
+}
+
+// MergeConfigWithMap 合并配置 map
+func MergeConfigWithMap(cfg map[string]interface{}) error {
+	return viper.MergeConfigMap(cfg)
+}
+
+// GetEnv 获取 系统环境变量
+func GetEnv(key string) interface{} {
+	viper.AutomaticEnv()
+	return viper.Get(key)
 }
