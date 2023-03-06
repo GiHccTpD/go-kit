c3ff075ff59be0e7b51057f67abe6dd21ef2b642
MiG <yanrancanfei@gmail.com>
2023-03-06 17:03:27 +0800

    feat(aes): 新增关闭提示

--- graceful_shutdown/signal.go
+++ graceful_shutdown/signal.go
@@ -4,11 +4,20 @@
 package graceful_shutdown
 
 import (
+	"fmt"
 	"github.com/GiHccTpD/go-kit/logger"
 	"os"
 	"os/signal"
 	"syscall"
 )
+
+const byebye = `  _______   __  __   ______        _______   __  __   ______      
+/_______/\ /_/\/_/\ /_____/\     /_______/\ /_/\/_/\ /_____/\     
+\::: _  \ \\ \ \ \ \\::::_\/_    \::: _  \ \\ \ \ \ \\::::_\/_    
+ \::(_)  \/_\:\_\ \ \\:\/___/\    \::(_)  \/_\:\_\ \ \\:\/___/\   
+  \::  _  \ \\::::_\/ \::___\/_    \::  _  \ \\::::_\/ \::___\/_  
+   \::(_)  \ \ \::\ \  \:\____/\    \::(_)  \ \ \::\ \  \:\____/\ 
+    \_______\/  \__\/   \_____\/     \_______\/  \__\/   \_____\/`
 
 var signalFuncList []func()
 
@@ -33,6 +42,7 @@ func WaitSignal() {
 				logger.Log.Debug("run")
 				s()
 			}
+			fmt.Printf("%v\n", fmt.Sprintf("\x1b[32m%s\x1b[0m", byebye))
 			return
 		}
 	}
