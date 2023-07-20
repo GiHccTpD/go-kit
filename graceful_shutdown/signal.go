115f95169ca33c3444c3b379e4bc1758b233dc01
MiG <yanrancanfei@163.com>
2023-07-20 11:02:38 +0800

    feat(gs): banner修改

--- graceful_shutdown/signal.go
+++ graceful_shutdown/signal.go
@@ -11,13 +11,11 @@ import (
 	"syscall"
 )
 
-const byebye = `  _______   __  __   ______        _______   __  __   ______      
-/_______/\ /_/\/_/\ /_____/\     /_______/\ /_/\/_/\ /_____/\     
-\::: _  \ \\ \ \ \ \\::::_\/_    \::: _  \ \\ \ \ \ \\::::_\/_    
- \::(_)  \/_\:\_\ \ \\:\/___/\    \::(_)  \/_\:\_\ \ \\:\/___/\   
-  \::  _  \ \\::::_\/ \::___\/_    \::  _  \ \\::::_\/ \::___\/_  
-   \::(_)  \ \ \::\ \  \:\____/\    \::(_)  \ \ \::\ \  \:\____/\ 
-    \_______\/  \__\/   \_____\/     \_______\/  \__\/   \_____\/`
+const banner = `  ____  _  _  ____     ____  _  _  ____ 
+(  _ \( \/ )(  __)___(  _ \( \/ )(  __)
+ ) _ ( )  /  ) _)(___)) _ ( )  /  ) _) 
+(____/(__/  (____)   (____/(__/  (____)
+`
 
 var signalFuncList []func()
 
@@ -32,7 +30,7 @@ func AddSignalFunc(signalFunc func()) {
 func WaitSignal() {
 	log.Println("wait signal")
 	c := make(chan os.Signal, 1)
-	
+
 	// kill 默认会发送 syscall.SIGTERM 信号
 	// kill -2 发送 syscall.SIGINT 信号，我们常用的 CTRL + C 就是触发系统 SIGINT 信号
 	// kill -9 发送 syscall.SIGKILL 信号，但是不能被捕获，所以不需要添加它
@@ -46,7 +44,7 @@ func WaitSignal() {
 				log.Println("run")
 				s()
 			}
-			fmt.Printf("%v\n", fmt.Sprintf("\x1b[32m%s\x1b[0m", byebye))
+			fmt.Printf("%v\n", fmt.Sprintf("\x1b[32m%s\x1b[0m", banner))
 			return
 		}
 	}
